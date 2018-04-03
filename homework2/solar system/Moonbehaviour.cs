fb69c200f6a8a05c8d1448349b7ef0b5d3996a09
zhongshuaihui <31625530+zhongshuaihui@users.noreply.github.com>
2018-04-03 16:49:03 +0800

    Add files via upload

---
+++ homework2/solar system/Moonbehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moonbehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = this.transform.parent.position;
+        this.transform.RotateAround(position, Vector3.up, 200 * Time.deltaTime);
+	}
+}
---
+++ homework2/solar system/emptybehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emptybehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(Vector3.zero, new Vector3(0, 1, 1), 60 * Time.deltaTime);
+
+    }
+}
