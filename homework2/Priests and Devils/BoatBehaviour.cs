5e5ec5f29acfb3438942dc1c1f4e7f781977557d
zhongshuaihui <31625530+zhongshuaihui@users.noreply.github.com>
2018-04-03 16:46:24 +0800

    Add files via upload

---
+++ homework2/Priests and Devils/BoatBehaviour.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.Mygame;
+
+public class BoatBehaviour : MonoBehaviour {
+
+    private Vector3 moveDir = new Vector3(-0.1f, 0, 0);
+    public bool ismoving;
+    public bool atleftside;
+    public bool leftEmpty, rightEmpty;
+    private GameJudge gamejudge;
+
+    // Use this for initialization
+    void Start () {
+        ismoving = false;
+        atleftside = direction.right;
+        leftEmpty = true;
+        rightEmpty = true;
+        gamejudge = mainController.getInstance() as GameJudge;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ismoving)
+            if (!ismovingtoedge())
+                this.transform.Translate(moveDir);
+	}
+
+    private bool ismovingtoedge()
+    {
+        if (moveDir.x < 0 && this.transform.position.x <= location.boat_left_loc.x)
+        {
+            ismoving = false;
+            atleftside = direction.left;
+            gamejudge.ifgg(direction.left);
+            moveDir = new Vector3(-moveDir.x, 0, 0);
+            return true;
+        }
+        else if (moveDir.x > 0 && this.transform.position.x >= location.boat_right_loc.x)
+        {
+            ismoving = false;
+            atleftside = direction.right;
+            gamejudge.ifgg(direction.right);
+            moveDir = new Vector3(-moveDir.x, 0, 0);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void setboatmove()
+    {
+        if (!ismoving && (!leftEmpty || !rightEmpty))
+            ismoving = true;
+    }
+
+    public bool boatloc()
+    {
+        return atleftside;
+    }
+
+    public bool leftseatempty()
+    {
+        return leftEmpty;
+    }
+
+    public bool rightseatempty()
+    {
+        return rightEmpty;
+    }
+
+    public void haveaseat(bool isleft)
+    {
+        if (isleft)
+            leftEmpty = false;
+        else
+            rightEmpty = false;
+    }
+
+    public void leaveseat(bool isleft)
+    {
+        if (isleft)
+            leftEmpty = true;
+        else
+            rightEmpty = true;
+    }
+}
---
+++ homework2/Priests and Devils/PersonStatus.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.Mygame;
+
+public class PersonStatus : MonoBehaviour {
+
+    private Vector3 originalpos;
+    public bool onboatleft, onboatright;
+    public bool onleftbank, onrightbank;
+    private GameJudge gamejudge;
+
+	// Use this for initialization
+	void Start () {
+        originalpos = this.transform.position;
+        onboatleft = false;
+        onboatright = false;
+        onleftbank = false;
+        onrightbank = true;
+        gamejudge = mainController.getInstance() as GameJudge;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void persongetonboat(bool boatatleft,bool leftseat)
+    {
+        if (leftseat)
+        {
+            if (boatatleft)
+                this.transform.position = location.boatLeft_1_loc;
+            else
+                this.transform.position = location.boatRight_1_loc;
+            onboatleft = true;
+        }
+        else
+        {
+            if (boatatleft)
+                this.transform.position = location.boatLeft_2_loc;
+            else
+                this.transform.position = location.boatRight_2_loc;
+            onboatright = true;
+        }
+        onleftbank = false;
+        onrightbank = false;
+        if(this.tag.Equals("Priest"))
+        {
+            gamejudge.change_boat_priests_num(ifadd.add);
+            gamejudge.change_bank_priests_num(boatatleft, ifadd.sub);
+        }
+        else
+        {
+            gamejudge.change_boat_devils_num(ifadd.add);
+            gamejudge.change_bank_devils_num(boatatleft, ifadd.sub);
+        }
+    }
+
+    public void persongetoffboat(bool boatatleft)
+    {
+        if (boatatleft)
+        {
+            this.transform.position = new Vector3(-originalpos.x, originalpos.y, originalpos.z);
+            onleftbank = true;
+        }
+        else
+        {
+            this.transform.position = originalpos;
+            onrightbank = true;
+        }
+        onboatleft = false;
+        onboatright = false;
+        if (this.tag.Equals("Priest"))
+        {
+            gamejudge.change_boat_priests_num(ifadd.sub);
+            gamejudge.change_bank_priests_num(boatatleft, ifadd.add);
+        }
+        else
+        {
+            gamejudge.change_boat_devils_num(ifadd.sub);
+            gamejudge.change_bank_devils_num(boatatleft, ifadd.add);
+        }
+    }
+}
