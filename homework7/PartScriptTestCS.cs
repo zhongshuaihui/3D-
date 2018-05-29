2b8dc7747fb9054eec1159b5d8adbb9e5f952ac1
zhongshuaihui <31625530+zhongshuaihui@users.noreply.github.com>
2018-05-29 19:53:09 +0800

    Add files via upload

---
+++ homework7/PartScriptTestCS.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartScriptTestCS : MonoBehaviour
+{
+
+    public float engineRevs;
+    public float exhaustRate;
+
+    ParticleSystem exhaust;
+
+
+    void Start()
+    {
+        exhaust = GetComponent<ParticleSystem>();
+    }
+
+
+    void Update()
+    {
+        exhaust.emissionRate = engineRevs * exhaustRate;
+    }
+
+}
